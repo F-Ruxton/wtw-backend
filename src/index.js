9d07e0d41436e1000321622f2cc619de00050a43
Freddie Ruxton <freddie.ruxton@procensus.com>
2018-11-29 17:16:34 +0000

    Remove old server config

--- src/index.js
+++ src/index.js
@@ -10,14 +10,3 @@ middleware.install(app);
 apis.register(app);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
